4ad20de8c53fc23af32bf51b9509243413735f9b
Momen Ashraf <119715732+Moamen-Ashraf@users.noreply.github.com>
2024-12-29 11:11:44 +0200

    [1045] [Time Beats: 92.79%] - LeetPush

---
+++ SQL 50/1045-Customers-Who-Bought-All-Products.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT C.customer_id
+FROM Customer C INNER JOIN Product P
+ON C.product_key = P.product_key
+GROUP BY C.customer_id
+HAVING COUNT(DISTINCT C.product_key) >= (SELECT COUNT(*) FROM Product)
